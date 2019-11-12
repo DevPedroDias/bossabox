b50b2df2baa81029b8356996459e54e9c7463e62
Pedro Elpídio Correa Dias <pedrodias@ENGEFT01.LOCAL>
2019-11-12 13:41:59 -0300

    first push

---
+++ database/factories/ToolsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+use App\Tool as Tool;
+$factory->define(Tool::class, function (Faker $faker) {
+    return [
+
+    ];
+});
