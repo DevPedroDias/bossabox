c0da65c8df988078a10e654a32b72c7a1618eee3
Pedro Elpídio Correa Dias <pedrodias@ENGEFT01.LOCAL>
2019-11-13 11:33:12 -0300

    frontend started

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
 require('./bootstrap');
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+Vue.use(VueAxios, axios);
+
+
+import App from './components/App.vue';
+import Tools from './components/Tools.vue';
+
+
+const routes = [
+    {
+        name: 'Tools',
+        path: '/',
+        component: Tools
+    }
+  ];
+
+const router = new VueRouter({ mode: 'history', routes: routes});
+new Vue(Vue.util.extend({ router }, App)).$mount('#app');
---
+++ resources/js/components/Tools.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>Oi</h2>
+        <div v-for="tool in tools" v-bind:key="tool.id">
+            {{tool.title}}
+        </div>
+    </div>
+</template>
+<script>
+export default {
+data(){
+    return{
+        'tools': []
+    }
+},
+mounted(){
+    this.getTools();
+},
+methods:{
+    getTools(){
+        axios.get('api/tools')
+        .then(response => {
+            this.tools = response.data;
+        })
+        .catch(error =>{
+            console.log(error)
+        });
+    }
+}
+}
+</script>
